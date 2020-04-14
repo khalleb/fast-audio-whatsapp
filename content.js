f22c07269e090b732db237cd7f529bc20abf2b27
khalleb <khallebw@gmail.com>
2020-04-14 20:10:40 -0300

    first commit

---
+++ content.js
@@ -0,0 +1,19 @@
+const inteval = setInterval(() => {
+  const header = document.querySelector("._3auIg");
+  if (header) {
+    clearInterval(inteval);
+    const button = document.createElement("button");
+    button.innerHTML = "2x";
+    button.classList.add("twoTimesButton");
+
+    button.addEventListener("click", () => {
+      const audios = document.querySelectorAll("audio");
+      audios.forEach((audio) => {
+        audio.playbackRate = 2;
+      })
+    })
+
+    header.appendChild(button);
+
+  }
+}, 1000)
